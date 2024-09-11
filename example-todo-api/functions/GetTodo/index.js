8149023bbbb0e6a41b570d323ee0c89528d4fc82
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2024-09-11 11:49:43 +0200

    Added code

---
+++ example-todo-api/functions/GetTodo/index.js
@@ -0,0 +1,21 @@
+const { sendResponse } = require("../../responses/index");
+const { db } = require("../../services/db");
+
+module.exports.handler = async (event) => {
+  const { username } = event.pathParameters;
+  console.log(username);
+  console.log(event.pathParameters);
+
+  try {
+    const { Items } = await db.query({
+      TableName: "example-todos",
+      IndexName: "usernameIndex",
+      KeyConditionExpression: "username = :username",
+      ExpressionAttributeValues: {
+        ":username": username,
+      },
+    });
+
+    return sendResponse(Items);
+  } catch (error) {}
+};
